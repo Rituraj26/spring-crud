5673840f97ee08bab386e0e7d061e7521ea58ba4
Rituraj <riturajd265@gmail.com>
2021-10-17 23:24:16 +0530

    get mapping and post mapping

---
+++ src/main/java/com/crud/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crud.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	
+}
