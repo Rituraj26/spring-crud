9638b9c2d5e58792b29457f8582c9d6e0bbe9eb6
Rituraj <riturajd265@gmail.com>
2021-10-30 18:33:33 +0530

    put and delete mapping

--- src/main/java/com/crud/controller/BooksController.java
+++ src/main/java/com/crud/controller/BooksController.java
@@ -1,15 +1,18 @@
 package com.crud.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.crud.repository.BookRepository;
+import com.crud.exception.ResourceNotFoundException;
 import com.crud.model.Book;
+import com.crud.repository.BookRepository;
 
 @RestController
 public class BooksController {
@@ -27,5 +30,24 @@ public class BooksController {
 	{
 		bookRepository.save(book);
 		return book;
+	}
+	
+	@PutMapping("/book/{id}")
+	public Book editBook(@PathVariable int id, @RequestBody Book book)
+	{
+		Book editedBook = bookRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Book not found"));
+		editedBook.setBookName(book.getBookName());
+		Book updatedBook = bookRepository.save(editedBook);
+		return updatedBook;
+	}
+	
+	@DeleteMapping("/book/{id}")
+	public Book deleteBook(@PathVariable int id)
+	{
+		Book deletedBook = bookRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Book not found"));
+		bookRepository.delete(deletedBook);
+		return deletedBook;
 	}
 }
